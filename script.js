a2c93b8910026a522ec68a67b4efe17f597b6b0c
arine <ariangerris@gmail.com>
2024-09-29 03:42:13 +0400

    added swiper

--- script.js
+++ script.js
@@ -14,60 +14,21 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-  const controls = document.querySelectorAll(".feature-control");
-  const featureImage = document.getElementById("feature-image");
-  let currentFeature = 0;
-  let startX = 0;
-
-  function showFeature(index) {
-    controls.forEach((control, i) => {
-      control.classList.toggle("active", i === index);
-    });
-    const newImage = controls[index].getAttribute("data-image");
-    featureImage.src = `./img/${newImage}`;
-  }
-
-  document.querySelectorAll(".arrow-left").forEach((arrow) => {
-    arrow.addEventListener("click", function () {
-      currentFeature = (currentFeature - 1 + controls.length) % controls.length;
-      showFeature(currentFeature);
-    });
+  const swiper = new Swiper(".swiper-container", {
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    pagination: false,
+    navigation: false
   });
 
-  document.querySelectorAll(".arrow-right").forEach((arrow) => {
-    arrow.addEventListener("click", function () {
-      currentFeature = (currentFeature + 1) % controls.length;
-      showFeature(currentFeature);
-    });
+  document.querySelector(".arrow-left").addEventListener("click", function () {
+    swiper.slidePrev();
   });
 
-  const featuresContainer = document.querySelector(".features-mobile-images");
-
-  featuresContainer.addEventListener("touchstart", function (e) {
-    startX = e.touches[0].clientX;
-    e.preventDefault();
+  document.querySelector(".arrow-right").addEventListener("click", function () {
+    swiper.slideNext();
   });
-
-  featuresContainer.addEventListener("touchmove", function (e) {
-    e.preventDefault();
-    const currentX = e.touches[0].clientX;
-    const diffX = startX - currentX;
-
-    if (Math.abs(diffX) > 10) {
-      if (diffX > 0) {
-        currentFeature = (currentFeature + 1) % controls.length;
-      } else {
-        currentFeature =
-          (currentFeature - 1 + controls.length) % controls.length;
-      }
-      showFeature(currentFeature);
-      startX = currentX;
-    }
-  });
-
-  featuresContainer.addEventListener("touchend", function () {
-    startX = 0;
-  });
-
-  showFeature(currentFeature);
 });
